ebab8d623ee0a92666ba94e378db6e39584c42e4
lollol23 <mark6262@naver.com>
2021-07-22 15:48:20 +0900

    [임윤섭] -servlet get method 일부 예제 추가

---
+++ src/main/java/com/lollol23/servlet/ServletEx03.java
@@ -0,0 +1,30 @@
+package com.lollol23.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//http://localhostt:8080/servlet/ex03?user_id=lollol23&name=lol
+@WebServlet("/servlet/ex03")
+public class ServletEx03 extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("user_id");
+		String name = request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><head><title>get 파라미터확인</title></head>");
+		out.println("<body><h3>" + id + " : " + name + " : " + age + "</h3></body><html>");
+	}
+
+}
