3d782fafaf469f5c018453eea4d104299d3861e4
lollol23 <mark6262@naver.com>
2021-07-26 15:00:09 +0900

    [임윤섭] -servletTest08 문제풀이 추가

---
+++ src/main/java/com/lollol23/servlet/servletTest08.java
@@ -0,0 +1,44 @@
+package com.lollol23.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test08")
+public class servletTest08 extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String serchWord = request.getParameter("serch_word");
+		
+		List<String> list = new ArrayList<>(Arrays.asList(
+		        "강남역 최고 맛집 소개 합니다.", 
+		        "오늘 기분 좋은 일이 있었네요.", 
+		        "역시 맛집 데이트가 제일 좋네요.", 
+		        "집에 가는 길에 동네 맛집 가서 안주 사갑니다.",
+		        "자축 저 오늘 생일 이에요."));
+		
+		out.println("<html><head><title>검색하기</title></head><body>");
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).contains(serchWord)) {
+				String newText = list.get(i).replace(serchWord, "<b>" + serchWord + "</b>");
+				out.println(newText + "<hr>");
+			}
+		}
+		out.println("</body></html>");
+		
+		
+	}
+}
