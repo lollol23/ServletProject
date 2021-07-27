7bc74d0436d4a24fb9e82272e17820ff7bbffdac
lollol23 <mark6262@naver.com>
2021-07-27 14:32:18 +0900

    [임윤섭] -post method 예제 풀이 및 servlet test09 문제 풀이 추가

---
+++ src/main/java/com/lollol23/servlet/servletEx05.java
@@ -0,0 +1,39 @@
+package com.lollol23.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/ex05")
+public class servletEx05 extends HttpServlet{
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String userId = request.getParameter("user_id");
+		String name = request.getParameter("name");
+		String birth = request.getParameter("birth");
+		String email = request.getParameter("email");
+		
+		//테이블 형태로 출력
+		out.println("<html><head><title>회원정보</title></head>");
+		out.println("<body><table border=1>");
+		//이름 : 롤롤
+		//생년월일 : 19990909
+		out.println("<tr><td>아이디</td> <td>" + userId + "</td></tr>");
+		out.println("<tr><td>이름</td> <td>" + name + "</td></tr>");
+		out.println("<tr><td>생년월일</td> <td>" + birth + "</td></tr>");
+		out.println("<tr><td>이메일</td> <td>" + email + "</td></tr>");
+
+		
+		out.println("</table></body></html>");
+	}
+}
---
+++ src/main/java/com/lollol23/servlet/servletTest09.java
@@ -0,0 +1,29 @@
+package com.lollol23.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test09")
+public class servletTest09 extends HttpServlet{
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String introduce = request.getParameter("introduce");
+		
+		out.println("<html><head><title>지원하기</title></head><body>");
+		out.println("<h2>" + name + "님 지원이 완료 되었습니다</h2><hr>");
+		out.println("지원내용<br>");
+		out.println(introduce + "</body></html>");
+	}
+}
